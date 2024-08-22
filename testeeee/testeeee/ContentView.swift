fb6f490e9a2362564c0b115b8670b46a121cff47
Ana Paula Sá <anapaulasapaiva@gmail.com>
2024-08-22 15:15:25 -0300

    nada' :q q .q qjhek

---
+++ testeeee/testeeee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testeeee
+//
+//  Created by Ana Paula Sá Barreto Paiva da Cunha on 05/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testeeee/testeeee/testeeeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeeeeApp.swift
+//  testeeee
+//
+//  Created by Ana Paula Sá Barreto Paiva da Cunha on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct testeeeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
